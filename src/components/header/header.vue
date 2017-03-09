4df20906e24c56827b3d812227c96be87ac4933b
niuzz <niuzz@hotmail.com>
2017-03-09 23:03:30 +0800

    before vue-router

---
+++ src/components/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">
+        <h1>
+            我是header
+        </h1>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {};
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
