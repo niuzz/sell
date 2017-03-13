447d14850e2fd16dea4834513f3aefdf42c9f13f
niuzz <niuzz@hotmail.com>
2017-03-13 22:56:01 +0800

    外部组件4

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,16 +1,99 @@
 <template>
     <div class="header">
-        <h1>
-            我是header
-        </h1>
+        <div class="content-wraper">
+            <div class="avatar">
+                <img :src="seller.avatar" alt="avatar" width="64" height="64">
+            </div>
+            <div class="content">
+                <div class="title">
+                    <span class="brand"></span>
+                    <span class="name">{{ seller.name }}</span>
+                </div>
+                <div class="description">
+                    {{ seller.description }}/{{ seller.deliveryTime }}分钟送达
+                </div>
+                <!-- 如果不写if则有可能是传过来的是空对象,因为数据是异步加载的 -->
+                <div v-if="seller.supports" class="support">
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+                    <span class="text"> {{ seller.supports[0].description }} </span>
+
+                </div>
+            </div>
+        </div>
+        <div class="bulletin-wraper">
+
+        </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {};
+    export default {
+        props: {
+            seller: {
+                type: Object
+            }
+        },
+        created () {
+            this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
+        }
+    };
 
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/stylus/mixin.styl"
+    .header
+        color: #fff
+        background: #000
+        .content-wraper
+            padding: 24px 12px 18px 24px
+            font-size: 0px
+            .avatar
+                display: inline-block
+                vertical-align: top
+            .content
+                display inline-block
+                margin-left: 16px
+                font-size: 0px
+                .title
+                    margin: 2px 0 8px 0
+                    .brand
+                        display: inline-block
+                        vertical-align: top
+                        width: 30px
+                        height: 18px
+                        bg-image("brand")
+                        background-size: 30px 18px
+                        background-repeat: no-repeat
+                    .name
+                        margin-left: 16px
+                        font-size: 16px
+                        line-height: 18px
+                        font-weight: bold
+                .description
+                    margin-bottom: 10px
+                    line-height: 12px
+                    font-size: 12px
+                .support
+                    .icon
+                        display: inline-block
+                        width: 12px
+                        height: 12px
+                        vertical-align: top
+                        margin-right: 4px
+                        background-size: 12px 12px
+                        &.decrease
+                            bg-image('decrease_1')
+                        &.discount
+                            bg-image('discount_1')
+                        &.guarantee
+                            bg-image('guarantee_1')
+                        &.invoice
+                            bg-image('invoice_1')
+                        &.special
+                            bg-image('special_1')
+                    .text
+                        line-height: 12px
+                        font-size: 10px
 
 </style>
