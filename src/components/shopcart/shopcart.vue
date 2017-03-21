7f48c9dfdb2ef8f520d42b0f76c0b5a07453de27
niuzz <niuzz@hotmail.com>
2017-03-21 21:57:07 +0800

    shopcart2

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -5,18 +5,15 @@
                 <div class="content-left">
                     <div class="logo-wrapper">
                         <div class="logo">
-                            <i class="icon-shoping_cart">
-
-                            </i>
-                            <div class="num"></div>
-
+                            <i class="icon-shopping_cart"></i>
                         </div>
+                        <div class="num" v-show="totalCount>0">{{totalCount}}</div>
                     </div>
-                    <div class="price"></div>
+                    <div class="price">￥{{totalPrice}}</div>
                     <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
                 </div>
                 <div class="content-right">
-                    <div class="pay">
+                    <div class="pay" :class="payClass">
                         {{payDesc}}
                     </div>
                 </div>
@@ -41,6 +38,10 @@
                 }
             },
             deliveryPrice: {
+                type: Number,
+                default: 0
+            },
+            minPrice: {
                 type: Number,
                 default: 0
             }
@@ -68,6 +69,13 @@
                     return `还差${diff}元起送`;
                 } else {
                     return '去结算';
+                }
+            },
+            payClass () {
+                if (this.totalPrice < this.minPrice) {
+                    return 'not-enough';
+                } else {
+                    return 'enough';
                 }
             }
         }
@@ -117,50 +125,50 @@
                             color: #80858a
                             &.highlight
                                 color: #ffffff
-                        .num
-                            position: absolute
-                            top: 0
-                            right: 0
-                            width: 24px
-                            height: 16px
-                            line-height: 16px
-                            text-align: center
-                            border-radius: 16px
-                            font-size: 9px
-                            font-weight: 700
-                            color: #fff
-                            background: rgb(240, 20, 20)
-                            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
-                    .price
-                        display: inline-block
-                        vertical-align: top
-                        margin-top: 12px
-                        line-height: 24px
-                        padding-right: 12px
-                        box-sizing: border-box
-                        border-right: 1px solid rgba(255, 255, 255, 0.1)
-                        font-size: 16px
-                        font-weight: 700
-                        &.highlight
-                            color: #ffffff
-                    .desc
-                        display: inline-block
-                        vertical-align: top
-                        margin: 12px 0 0 12px
-                        line-height: 24px
-                        font-size: 10px
-                .content-right
-                    flex: 0 0 105px
-                    width: 105px
-                    .pay
-                        height: 48px
-                        line-height: 48px
+                    .num
+                        position: absolute
+                        top: 0
+                        right: 0
+                        width: 24px
+                        height: 16px
+                        line-height: 16px
                         text-align: center
-                        font-size: 12px
+                        border-radius: 16px
+                        font-size: 9px
                         font-weight: 700
-                        &.not-enough
-                            background: #2b333b
-                        &.enough
-                            background: #00b43c
-                            color: #ffffff
+                        color: #fff
+                        background: rgb(240, 20, 20)
+                        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
+                .price
+                    display: inline-block
+                    vertical-align: top
+                    margin-top: 12px
+                    line-height: 24px
+                    padding-right: 12px
+                    box-sizing: border-box
+                    border-right: 1px solid rgba(255, 255, 255, 0.1)
+                    font-size: 16px
+                    font-weight: 700
+                    &.highlight
+                        color: #ffffff
+                .desc
+                    display: inline-block
+                    vertical-align: top
+                    margin: 12px 0 0 12px
+                    line-height: 24px
+                    font-size: 10px
+            .content-right
+                flex: 0 0 105px
+                width: 105px
+                .pay
+                    height: 48px
+                    line-height: 48px
+                    text-align: center
+                    font-size: 12px
+                    font-weight: 700
+                    &.not-enough
+                        background: #2b333b
+                    &.enough
+                        background: #00b43c
+                        color: #ffffff
 </style>
