42486be6d7211716518b9b36eafffcebedc77e20
niuzz <niuzz@hotmail.com>
2017-03-27 17:12:55 +0800

    add food compenent

---
+++ src/components/food/food.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-show="showFlag" class="food">
+
+    </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    props: {
+        food: {
+           type: Object
+        }
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
