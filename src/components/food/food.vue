a10e6c49eff6b682c0a0524efbb924ef038b3b81
niuzz <niuzz@hotmail.com>
2017-03-28 17:57:26 +0800

    food2

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -35,6 +35,19 @@ export default {
         z-index: 30
         width: 100%
         background: #ffffff
+        transform: translate3d(0, 0, 0)
+        .image-header
+            position: relative
+            width: 100%
+            height: 0
+            position-top: 100%
+            img
+                position: absolute
+                top: 0
+                left: 0
+                width: 100%
+                height: 100%
+
     }
 
 </style>
