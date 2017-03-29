d4fc24ad0c5e67880a9e4c02cda6433b85528170
niuzz <niuzz@hotmail.com>
2017-03-29 09:25:56 +0800

    food-content-title-price etc

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -7,6 +7,16 @@
                     <i class="icon-arrow_lift"></i>
                 </div>
             </div>
+            <div class="content">
+                <h1 class="title">{{food.name}}</h1>
+                <div class="detail">
+                    <span class="sell-count">月售{{food.sellCount}}</span>
+                    <span class="rating">好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                    <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+            </div>
         </div>
 
     </div>
@@ -68,5 +78,37 @@ export default {
                     padding: 10px
                     font-size: 20px;
                     color: #ffffff
+
+
+        .content
+            position: relative
+            padding: 18px
+            .title
+                line-height: 14px
+                margin-bottom: 8px
+                font-size: 14px
+                font-weight: 700
+                color: rgb(7, 17, 27)
+            .detail
+                margin-bottom: 18px
+                line-height: 10px
+                height: 10px
+                font-size: 0
+                .sell-count, .rating
+                    font-size: 10px;
+                    color: rgb(147, 153, 159)
+                .sell-count
+                    margin-right: 12px
+            .price
+                font-weight: 700
+                lint-height: 24px
+                .now
+                    margin-right: 8px
+                    font-size: 14px
+                    color: rgb(240, 20, 20)
+                .old
+                    text-decoration: line-through
+                    font-size: 10px
+                    color: rgb(147, 153, 159)
 
 </style>
