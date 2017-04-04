eb600d2eb409ccffa3750a8d09aae10a29225d80
niuzz <niuzz@hotmail.com>
2017-04-04 20:48:29 +0800

    food-ratingselect-dateFormat

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,49 +1,62 @@
 <template>
-    <div v-show="showFlag" class="food" ref="food">
-        <div class="food-content">
-            <div class="image-header">
-                <img :src="food.image">
-                <div class="back" @click="hide">
-                    <i class="icon-arrow_lift"></i>
-                </div>
-            </div>
-            <div class="content">
-                <h1 class="title">{{food.name}}</h1>
-                <div class="detail">
-                    <span class="sell-count">月售{{food.sellCount}}</span>
-                    <span class="rating">好评率{{food.rating}}%</span>
-                </div>
-                <div class="price">
-                    <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
-                </div>
-                <div class="cartcontrol-wrapper">
-                    <cartcontrol @add="addFood" :food="food"></cartcontrol>
-                </div>
-                <transition name="fade">
-                    <div @click.stop.prevent = "addFirst" class="buy" v-show="!food.count || food.count === 0">
-                        加入购物车
+    <transition name="move">
+        <div v-show="showFlag" class="food" ref="food">
+            <div class="food-content">
+                <div class="image-header">
+                    <img :src="food.image">
+                    <div class="back" @click="hide">
+                        <i class="icon-arrow_lift"></i>
                     </div>
-                </transition>
-            </div>
-            <split v-show="food.info"></split>
-            <div class="info" v-show="food.info">
-                <h1 class="title">商品信息</h1>
-                <p class="text">{{food.info}}</p>
-            </div>
-            <split></split>
-            <div class="rating">
-                <h1 class="title">商品评价</h1>
-                <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType"
-                              :onlyContent="onlyContent" :desc="desc"
-                              :ratings="food.ratings"></ratingselect>
-                <div class="rating-wrapper">
-
+                </div>
+                <div class="content">
+                    <h1 class="title">{{food.name}}</h1>
+                    <div class="detail">
+                        <span class="sell-count">月售{{food.sellCount}}</span>
+                        <span class="rating">好评率{{food.rating}}%</span>
+                    </div>
+                    <div class="price">
+                        <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                    </div>
+                    <div class="cartcontrol-wrapper">
+                        <cartcontrol @add="addFood" :food="food"></cartcontrol>
+                    </div>
+                    <transition name="fade">
+                        <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count === 0">
+                            加入购物车
+                        </div>
+                    </transition>
+                </div>
+                <split v-show="food.info"></split>
+                <div class="info" v-show="food.info">
+                    <h1 class="title">商品信息</h1>
+                    <p class="text">{{food.info}}</p>
+                </div>
+                <split></split>
+                <div class="rating">
+                    <h1 class="title">商品评价</h1>
+                    <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType"
+                                  :onlyContent="onlyContent" :desc="desc"
+                                  :ratings="food.ratings"></ratingselect>
+                    <div class="rating-wrapper">
+                        <ul v-show="food.ratings && food.ratings.length">
+                            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings"
+                                class="rating-item border-1px">
+                                <div class="user">
+                                    <span class="name">{{rating.username}}</span>
+                                    <img :src="rating.avatar" class="avatar" width="12" height="12">
+                                </div>
+                                <div class="time">{{rating.rateTime | formatDate}}</div>
+                                <p class="text">
+                                    <span :class="{'icon-thumb_up':rating.rateType ===0, 'icon-thumb_down':rating.rateType ===1}"></span>{{rating.text}}
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+                    </div>
                 </div>
             </div>
         </div>
-
-    </div>
-
+    </transition>
 </template>
 
 <script type="text/ecmascript-6">
@@ -52,7 +65,7 @@
     import cartcontrol from 'components/cartcontrol/cartcontrol';
     import ratingselect from 'components/ratingselect/ratingselect';
     import split from 'components/split/split';
-
+    import {formatDate} from 'common/js/date';
     const ALL = 2;
 
     export default {
@@ -81,7 +94,7 @@
                 this.$nextTick(() => {
                     if (!this.scroll) {
                         this.scroll = new BScroll(this.$refs.food, {
-                           click: true
+                            click: true
                         });
                     } else {
                         this.scroll.refresh();
@@ -112,6 +125,22 @@
                 this.$nextTick(() => {
                     this.scroll.refresh();
                 });
+            },
+            needShow (type, text) {
+                if (this.onlyContent && !text) {
+                    return false;
+                }
+                if (this.selectType === ALL) {
+                    return true;
+                } else {
+                    return type === this.selectType;
+                }
+            }
+        },
+        filters: {
+            formatDate (time) {
+                let date = new Date(time);
+                return formatDate(date, 'yyyy-MM-dd hh:mm');
             }
         },
         components: {
@@ -134,6 +163,10 @@
         width: 100%
         background: #ffffff
         transform: translate3d(0, 0, 0)
+        &.move-enter-avtive, &.move-leave-active
+            transition: all 0.2s linear
+        &.move-enter, &.move-leave-active
+            transform: translate3d(100%, 0, 0)
         .image-header
             position: relative
             width: 100%
@@ -231,14 +264,42 @@
                 .rating-item
                     position: relative
                     padding: 16px 0
-                    border-1px(rgba(7,17,27,0.1))
+                    border-1px(rgba(7, 17, 27, 0.1))
                     .user
                         position: absolute
                         right: 0
                         top: 16px
                         line-height: 12px
                         font-size: 0
-                        .project-name
+                        .name
                             display: inline-block
+                            margin-right: 6px
+                            vertical-align: top
+                            font-size: 10px
+                            color: rgb(147, 153, 159)
+                        .avatar
+                            border-radius: 50%
+                    .time
+                        margin-bottom: 6px
+                        line-height: 12px
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
+                    .text
+                        line-height: 16px
+                        font-size: 12px
+                        color: rgb(7, 17, 27)
+                        .icon-thumb_up, .icon-thum_down
+                            margin-right: 4px
+                            line-height: 16px
+                            font-size: 12px
+                        .icon-thumb_up
+                            color: rgb(0, 160, 220)
+                        .icon-thumb_down
+                            color: rgb(147, 153, 159)
 
+
+                .no-rating
+                    padding: 16px 0
+                    font-size: 12px
+                    color: rgb(147, 153, 159)
 </style>
