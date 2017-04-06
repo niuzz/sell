da82324cfa1a36e54c5e7c6a4755d74c8645a545
niuzz <niuzz@hotmail.com>
2017-04-06 22:33:57 +0800

    seller -overview

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,12 +1,61 @@
 <template>
-    <div>
-        I'm seller
+    <div class="seller" ref="seller">
+        <div class="seller-content">
+            <div class="overview">
+                <h1 class="title">{{seller.name}}</h1>
+                <div class="desc border-1px">
+                    <star :size="36" :score="seller.score"></star>
+                    <span class="text">{{seller.ratingCount}}</span>
+                    <span class="text">月售{{seller.sellCount}}单</span>
+                </div>
+                <ul class="remark">
+                    <li class="block">
+                        <h2>起送价</h2>
+                        <div class="content">
+                            <span class="stress">{{seller.minPrice}}</span>
+                        </div>
+                    </li>
+                    <li class="block">
+                        <h2>商家配送</h2>
+                        <div class="content">
+                            <span class="stress">{{seller.deliveryPrice}}</span>
+                        </div>
+                    </li>
+                    <li class="block">
+                        <h2>平均配送时间</h2>
+                        <div class="content">
+                            <span class="stress">{{seller.deliveryTime}}</span>
+                        </div>
+                    </li>
+                </ul>
+                <div class="favorite" @click="toggleFavorite">
+                    <span class="icon-favorite" :class="{'active':favorite}"></span>
+                    <span class="text">{{favoriteText}}</span>
+                </div>
+            </div>
+            <split></split>
+            <div class="bulletin"></div>
+            <split></split>
+            <div class="pics"></div>
+            <split></split>
+            <div class="info"></div>
+        </div>
     </div>
 
 </template>
 
 <script type="text/ecmascript-6">
-    export default {};
+    import split from 'components/split/split';
+    export default {
+        props: {
+            seller: {
+                type: Object
+            }
+        },
+        components: {
+            split
+        }
+    };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
